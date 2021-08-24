3572255b172bd4b1975ddd8479d62efdeb552e2b
LZRS <lzrscg@protonmail.com>
2021-08-24 13:00:58 -0700

    run web without github token  and rename cdk backend stack

--- cdk/bin/backend.ts
+++ cdk/bin/backend.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { BackendStack } from '../lib/backend-stack';
+import { PrsBackendStack } from '../lib/prs-backend-stack';
 
 const app = new cdk.App();
-new BackendStack(app, 'BackendStack', {
+new PrsBackendStack(app, 'PrsBackendStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
